62b4efc56ce7a67c0c727e358e5eec11b99cee06
Hokuto Yamada <hokuto.yamada.j@gmail.com>
2020-08-18 01:47:24 +0900

    バッチ実行

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,4 +5,10 @@ class JobsController < ApplicationController
     @jobs = Job.all.order(fee_max: :desc)
     @job_count_by_company = Job.group(:company_name).count.sort_by(&:last).reverse
   end
+
+  def exec_batch
+    # heroku free dyno web だけでバッチ実行するため
+    Job.scrape
+    redirect_to jobs_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'jobs#index'
-  resources :jobs, only: :index
+  resources :jobs, only: :index do
+    get 'exec_batch', on: :collection
+  end
 end
