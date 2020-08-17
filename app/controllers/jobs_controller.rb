18c22219412cfd0bda7ce9c23b73773caa799584
Hokuto Yamada <hokuto.yamada.j@gmail.com>
2020-08-18 01:08:22 +0900

    項目名修正

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
 
   def index
-    @jobs = Job.all.order(pay_max: :desc)
+    @jobs = Job.all.order(fee_max: :desc)
     @job_count_by_company = Job.group(:company_name).count.sort_by(&:last).reverse
   end
 end
